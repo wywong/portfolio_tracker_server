85cc098bfe79066f249c25b3a2a1fe18b0128c2b
Wilson Y. Wong <Wilson.Young.Wong@gmail.com>
2019-10-05 22:29:27 -0700

    Add investment account table
    InvestmentAccount
    * id - id the account
    * name - name of the investment account (e.g. TFSA, RRSP, etc)
    * taxable - true if this account is a taxable investment account

--- flaskr/model.py
+++ flaskr/model.py
@@ -16,6 +16,7 @@ class StockTransaction(db.Model):
     cost_per_unit = db.Column(db.Integer, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     trade_fee = db.Column(db.Integer, nullable=False)
+    account_id = db.Column(db.Integer, db.ForeignKey('investment_account.id'))
 
     def __iter__(self):
         yield ('id', self.id)
@@ -24,3 +25,11 @@ class StockTransaction(db.Model):
         yield ('cost_per_unit', self.cost_per_unit)
         yield ('quantity', self.quantity)
         yield ('trade_fee', self.trade_fee)
+
+
+class InvestmentAccount(db.Model):
+    __tablename__ = "investment_account"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    taxable = db.Column(db.Boolean, nullable=False)
+    transactions = db.relationship('stock_transaction')
